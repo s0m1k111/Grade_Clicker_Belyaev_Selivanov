ee0d2aacaa6ee4a40e31474760ab07cc4697a207
s0m1k111 <spavel1207@gmail.com>
2026-03-11 10:42:07 +0300

    Step 5: Basic model and file

---
+++ app/src/main/java/com/belyaev/selivanov/gradeclicker/model/Grade.kt
@@ -0,0 +1,9 @@
+package com.belyaev.selivanov.gradeclicker.model
+
+import androidx.annotation.DrawableRes
+
+data class Grade(
+    @DrawableRes val imageId: Int,
+    val pointsPerClick: Int,
+    val threshold: Int
+)
